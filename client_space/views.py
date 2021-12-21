dca241a1674a3c098a25380bace7a8bf0fa78aaf
Alexander Filimonov <me@igeophysix.com>
2021-12-21 18:48:53 +0300

    index redirects to swagger-ui

--- client_space/views.py
+++ client_space/views.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.shortcuts import redirect
+from django.urls import reverse
 
 
 def index(request):
     if request.user.is_authenticated:
-        return redirect(f'{settings.FRONTEND_BASE_URL}/dashboard')
+        return redirect(reverse('swagger-ui'))
     else:
         return redirect(f'{settings.FRONTEND_BASE_URL}/login')
 
